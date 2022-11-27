3932cf57ea0ac3d54f572788c91c2197a4150f38
Patrix <patrixon9999@wp.pl>
2022-11-27 16:32:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# The Settlers 4 Gold Edition: PrintScreen Plugin
+
+This plugin is fixing black screenshot saved to the clipboard.  
+Loading it will cause the screen to be properly saved inside the clipboard.  
+**NOTE!** This plugin works only with The Settlers 4 Gold Edition (GoG version) (History Edition doesn't have this problem because it handles game window differently).
+
+## How to use
+
+You need an ASI Loader to use this mod. I recommend [The Settlers 4: ASI Loader](https://github.com/nyfrk/Settlers4-ASI-Loader) as it works nicely with the Gold and History Edition of The Settlers 4 and does not require any configuration. 
+
+1. [Download](https://github.com/Patrix9999/Settlers4-PrintScreen/releases) a release.
+2. Unpack the files into your `plugins` directory.
+3. Start the game. The mod will load automatically.
+
+To uninstall the mod remove `PrintScreen.asi` from your`plugins` directory. 
+
+# Contribute
+
+The official repository of this project is available at: https://github.com/Patrix9999/Settlers4-PrintScreen.
+
+Compile it yourself
+Download Visual Studio 2017 or 2019 with the C++ toolchain. The project is configured to build it with the Windows 10 compatible v143 toolchain. However, you should be able to change the toolchain to whatever you like. No additional libraries are required so it should compile out of the box.
---
+++ Settlers4-PrintScreen/dllmain.cpp
@@ -0,0 +1,104 @@
+#include "stdafx.h"
+
+BOOL WINAPI SaveScreenToClipboard()
+{
+	int x1 = GetSystemMetrics(SM_XVIRTUALSCREEN);
+	int y1 = GetSystemMetrics(SM_YVIRTUALSCREEN);
+	int x2 = GetSystemMetrics(SM_CXVIRTUALSCREEN);
+	int y2 = GetSystemMetrics(SM_CYVIRTUALSCREEN);
+	int w = x2 - x1;
+	int h = y2 - y1;
+
+	HDC hDC = GetDC(NULL);
+	HGDIOBJ hTempBitmap = GetCurrentObject(hDC, OBJ_BITMAP);
+
+	DeleteObject(hTempBitmap);
+
+	BITMAPFILEHEADER bfHeader = {};
+	bfHeader.bfType = (WORD)('B' | ('M' << 8));
+	bfHeader.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+
+	BITMAPINFOHEADER biHeader = {};
+	biHeader.biSize = sizeof(BITMAPINFOHEADER);
+	biHeader.biBitCount = 24;
+	biHeader.biCompression = BI_RGB;
+	biHeader.biPlanes = 1;
+	biHeader.biWidth = w;
+	biHeader.biHeight = h;
+
+	BITMAPINFO bInfo = {};
+	bInfo.bmiHeader = biHeader;
+
+	BYTE* bBits = NULL;
+	HDC srcDC = CreateCompatibleDC(hDC);
+	HBITMAP hBitmap = CreateDIBSection(hDC, &bInfo, DIB_RGB_COLORS, (VOID**)&bBits, NULL, 0);
+	SelectObject(srcDC, hBitmap);
+	BitBlt(srcDC, 0, 0, w, h, hDC, x1, y1, SRCCOPY);
+
+	// Create a new bitmap
+	HBITMAP hBitmap_copy = CreateBitmap(w, h, 1, 32, NULL);
+
+	// Copy the source bitmap to the new one    
+	HDC newDC = CreateCompatibleDC(hDC);
+
+	HBITMAP srcBitmap = (HBITMAP)SelectObject(srcDC, hBitmap);
+	HBITMAP newBitmap = (HBITMAP)SelectObject(newDC, hBitmap_copy);
+	BitBlt(newDC, 0, 0, w, h, srcDC, 0, 0, SRCCOPY);
+
+	DeleteDC(newDC);
+
+	// hBitmap_copy can now be copied to the clipboard
+	OpenClipboard(NULL);
+
+	EmptyClipboard();
+	SetClipboardData(CF_BITMAP, hBitmap_copy);
+
+	CloseClipboard();
+
+	DeleteDC(srcDC);
+	ReleaseDC(NULL, hDC);
+
+	DeleteObject(hBitmap);
+
+	return TRUE;
+}
+
+LPVOID __cdecl Hook_createScreenBitmap(LPVOID buffer, LPBITMAPINFO lpbmi, HGDIOBJ h, HBITMAP hbm, int bpp, char topToBottom);
+HOOK Ivk_createScreenBitmap AS(0x004E0A90, Hook_createScreenBitmap);
+LPVOID __cdecl Hook_createScreenBitmap(LPVOID buffer, LPBITMAPINFO lpbmi, HGDIOBJ h, HBITMAP hbm, int bpp, char topToBottom)
+{
+	Ivk_createScreenBitmap.Detach();
+
+	SaveScreenToClipboard();
+	LPVOID bBits = Ivk_createScreenBitmap(buffer, lpbmi, h, hbm, bpp, topToBottom);
+
+	Ivk_createScreenBitmap.Attach();
+
+	return bBits;
+}
+
+VOID WINAPI onDllAttach(HMODULE hModule)
+{
+}
+
+VOID WINAPI onDllDetach()
+{
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+		case DLL_PROCESS_ATTACH:
+			DisableThreadLibraryCalls(hModule);
+			onDllAttach(hModule);
+			break;
+
+		case DLL_PROCESS_DETACH:
+			onDllDetach();
+			break;
+	}
+
+	return TRUE;
+}
+
